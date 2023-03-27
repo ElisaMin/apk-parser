c5a78e419c7b4bb846bd675515aa696244834677
Heizi <xm.zhongjiaxin@gmail.com>
2023-03-28 03:36:54 +0800

    meaning less update

--- compose-desktop-ext/src/test/kotlin/run.kt
+++ compose-desktop-ext/src/test/kotlin/run.kt
@@ -1,31 +1,40 @@
 @file:JvmName("run")
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.sizeIn
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.singleWindowApplication
 import me.heizi.apk.parser.ktx.Image
 import net.dongliu.apk.parser.ApkFile
 import net.dongliu.apk.parser.bean.ApkIcon
 import kotlin.system.exitProcess
 
-fun main(args: Array<String>) {
+var time = System.currentTimeMillis()
+fun updateTime(msg:String="") = System.currentTimeMillis().also { println("Time: ${it - time}ms $msg") }.also { time = it }
 
+fun main(args: Array<String>) {
+    updateTime("init")
+    // init
     args.getOrNull(0)?.let {
+        updateTime("parse")
         ApkFile(it)
             .icons
             .takeIf { it.isNotEmpty() }
     } ?.let {icons->
+        updateTime("compose")
         singleWindowApplication {
             Row {
-                val icon = icons.minBy { it.density }
+                val icon = icons.filter { it.density>=0 }.minBy { it.density }
+                println(icon::class.simpleName)
+                icons.forEach {
+                    println(it::class)
+                    println(it.density)
+                    Image(it)
+                }
                 if (icon is ApkIcon.Adaptive) {
                     println(icon.background::class.simpleName)
-                    Image(icon, modifier = Modifier.sizeIn(minWidth = 128.dp, minHeight = 128.dp).fillMaxSize())
-                } else {
-                    Image(icon, modifier = Modifier.sizeIn(minWidth = 128.dp, minHeight = 128.dp).fillMaxSize())
+//                    Image(icon, modifier = Modifier.sizeIn(minWidth = 128.dp, minHeight = 128.dp).fillMaxSize())
+//                } else {
+//                    Image(icon, modifier = Modifier.sizeIn(minWidth = 128.dp, minHeight = 128.dp).fillMaxSize())
                 }
+                updateTime("draw")
             }
         }
 
